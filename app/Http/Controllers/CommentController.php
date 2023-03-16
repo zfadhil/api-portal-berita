8d4a59f94bbb1ec1c4dc5031d85127141a508e0c
FadhilYTProgrammer <fadhil18022007@gmail.com>
2023-03-16 14:35:24 +0700

    comment resource

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CommentResource;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function store(Request $request){
         $request -> validate([
-            'post_id' => 'required|exist:posts,id',
+            'post_id' => 'required|exists:posts,id',
             'comment_content' => 'required'
         ]);
+
+        $request['user_id'] = auth()->user()->id;
+
+        $comment = Comment::create($request->all());
+
+        // return response()->json($comment);
+        return new CommentResource($comment->loadMissing(['commentator:id,username']));
     }
 }
